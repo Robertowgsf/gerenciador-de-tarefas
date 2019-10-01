01101a563a491787d83a016a7cdf8fbebd67f6ce
Roberto Wagner <robertowgsf@gmail.com>
2019-10-01 02:28:08 -0300

    Added tarefa service

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { TarefaService } from './';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers: [
+    TarefaService
   ]
 })
 export class TarefasModule { }
